c12268181d2cf4cd347b72469213db468b714d68
Yusuf Yigit Aydemir <yusufyigitaydemir@gmail.com>
2023-05-29 22:19:52 +0300

    api calls

--- api calls/async await/app.js
+++ api calls/async await/app.js
@@ -1,5 +1,8 @@
 async function get(url){
     const response = await fetch(url)
     const data = await response.json()
+    console.log(data)
     return data
 }
+
+get("https://jsonplaceholder.typicode.com/posts")
